29406abec97bbbb48dedc1ff23255f220a889aff
Matheus <95648954+Tenorioos@users.noreply.github.com>
2022-05-20 07:55:09 -0300

    Initial commit

---
+++ src/app/tasks/task-list/task-list.component.ts
@@ -0,0 +1,22 @@
+import { TaskService } from './../shared/task.service';
+
+import { Component, OnInit } from '@angular/core';
+import { TaskFormComponent } from '../task-form/task-form.component';
+import { Task } from '../shared/task';
+
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent implements OnInit {
+    tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit() {
+    this.tasks = this.taskService.getAll();
+  }
+
+}
