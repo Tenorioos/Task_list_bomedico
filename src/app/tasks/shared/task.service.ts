3b739c8a88125a34db0fbd6656f66237d4ec3719
Tenorioos <95648954+Tenorioos@users.noreply.github.com>
2022-05-24 09:28:35 -0300

    send code

--- src/app/tasks/shared/task.service.ts
+++ src/app/tasks/shared/task.service.ts
@@ -6,22 +6,15 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TaskService {
-  tasks?: Task[] | undefined = [
-    { id: 1, description: 'Tarefa 1', completed: true },
-    { id: 2, description: 'Tarefa 2', completed: false },
-    { id: 3, description: 'Tarefa 3', completed: true },
-    { id: 4, description: 'Tarefa 4', completed: false },
-    { id: 5, description: 'Tarefa 5', completed: false },
-    { id: 6, description: 'Tarefa 6', completed: false },
-    { id: 7, description: 'Tarefa 7', completed: false },
-    { id: 8, description: 'Tarefa 8', completed: false },
-    { id: 9, description: 'Tarefa 9', completed: false },
-    { id: 10, description: 'Tarefa 10', completed: false },
-  ];
+  tasks?: Task[] | undefined = [];
 
   constructor() { }
 
   getAll(){
+   const list =  window.localStorage.getItem('lista-tarefas');
+    if(list){
+      this.tasks = JSON.parse(list);
+    }
     return this.tasks;
   }
 
@@ -36,15 +29,22 @@ export class TaskService {
       taskArr.description =  task.description;
       taskArr.completed =  task.completed;
       } else {
-        const lastId = this.tasks[this.tasks.length-1].id;
+        let lastId= 0;
+        if(this.tasks.length > 0){
+         lastId = this.tasks[this.tasks.length-1].id; 
+         }
         task.id = lastId + 1;
         task.completed = false;
         this.tasks.push(task);
       }
+      window.localStorage.setItem('lista-tarefas', JSON.stringify(this.tasks))
   }
   delete(id: number){
     const taskIndex = this.tasks.findIndex((value) => value.id == id);
     this.tasks.splice(taskIndex,1); // remover tarefa
+    window.localStorage.setItem('lista-tarefas', JSON.stringify(this.tasks))
   }
+
+
 
 }
